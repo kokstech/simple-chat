78e42d2f6ed69e985bd12923018713d28563239b
kokstech <stefan.ristic.koki@gmail.com>
2023-08-22 09:27:02 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const users = {};
+
+app.route("/").get(getPage).post(getPage);
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+function getPage(req, res) {
+  res.sendFile(__dirname + "/index.html");
+}
+
+server.listen(process.env.PORT, () => {
+  console.log("listening on: ", process.env.PORT);
+});
